8efce4e0592739666e95cdd90f0909c6430aeccb
rilind nuha <rn48707@ubt-uni.net>
2022-02-12 21:19:26 +0100

    feat(student): complete students CRUD

---
+++ src/validators/student.validation.ts
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+
+export const registerSchema = Joi.object({
+  studentId: Joi.number().required().label('Student ID'),
+  firstName: Joi.string().min(2).max(30).required().label('Firstname'),
+  lastName: Joi.string().min(2).max(30).required().label('Lastname'),
+  email: Joi.string().email().min(5).max(30).required().label('Email'),
+  password: Joi.string().alphanum().min(7).max(30).required().label('Password'),
+});
+
+export const updateSchema = Joi.object({
+  userId: Joi.string().required().label('ID'),
+  studentId: Joi.number().label('Student ID'),
+  firstName: Joi.string().min(2).max(30).label('Firstname'),
+  lastName: Joi.string().min(2).max(30).label('Lastname'),
+  email: Joi.string().email().min(5).max(30).label('Email'),
+  password: Joi.string().alphanum().min(7).max(30).label('Password'),
+});
