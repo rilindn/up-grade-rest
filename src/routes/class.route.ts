d5ca9e2382d499eb679b958de5137daa66d7b453
rilindaxhaqkaj <rilinda@centralsoft.io>
2022-02-14 22:23:27 +0100

    feat:classes CRUD

---
+++ src/routes/class.route.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import classController from '../controllers/class.controller';
+
+const classRoutes = express.Router();
+
+classRoutes.get('/', classController.getAllClasses);
+
+classRoutes.get('/:id', classController.getClassById);
+
+classRoutes.post('/create', classController.registerClass);
+
+classRoutes.put('/:id', classController.updateClass);
+
+classRoutes.delete('/:id', classController.deleteClass);
+
+
+export default classRoutes;
---
+++ src/validators/class.validation.ts
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+
+export const registerSchema = Joi.object({
+  level: Joi.number().min(1).max(12).required().label('Level'),
+  className: Joi.string().min(3).max(30).required().label('Classname'),
+  classCapacity: Joi.number().min(2).max(30).required().label('Classcapacity'),
+});
+
+export const updateSchema = Joi.object({
+  classId: Joi.string().required().label('ID'),
+  level: Joi.number().min(1).max(12).label('Level'),
+  className: Joi.string().min(3).max(30).label('Classname'),
+  classCapacity: Joi.number().min(2).max(30).label('Classcapacity'),
+});
