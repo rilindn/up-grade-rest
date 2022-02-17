6efbe2f39a890adc365c78ee9d4be8edd1517898
rilind nuha <rn48707@ubt-uni.net>
2022-02-17 21:27:05 +0100

    feat: add staff CRUD

---
+++ src/services/staff.service.ts
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import validateEmail from '../helpers/validateEmail';
+
+interface TeacherDTO {
+  firstName: String;
+  lastName: String;
+}
+
+export const generateStaffEmail = async (data: TeacherDTO) => {
+  const firstName = data?.firstName?.toLowerCase();
+  const lastName = data?.lastName?.toLowerCase();
+  let generatedEmail = `${firstName}.${lastName}@upgrade.edu`;
+  const validEmail = await validateEmail(generatedEmail);
+  if (validEmail) return generatedEmail;
+  else {
+    const randomKey = _.random(1, 9, false);
+    generatedEmail = `${firstName}.${lastName}${randomKey}@upgrade.edu`;
+    return generatedEmail;
+  }
+};
