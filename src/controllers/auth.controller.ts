107bcd218027fc8b21a8600cf9702c4d0d6caa43
rilind nuha <rn48707@ubt-uni.net>
2022-02-24 00:26:08 +0100

    fix: expire login token in 1 day

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,48 +1,48 @@
-import passport from 'passport';
-import jsonwebtoken from 'jsonwebtoken';
-import dotenv from 'dotenv';
-import User from '../models/user.model';
-import _ from 'lodash';
+import passport from 'passport'
+import jsonwebtoken from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import User from '../models/user.model'
+import _ from 'lodash'
 
-dotenv.config();
-const jwt = jsonwebtoken;
+dotenv.config()
+const jwt = jsonwebtoken
 
 const login = async (req: any, res: any, next: any) => {
   passport.authenticate('local', async (err: any, user: any) => {
     try {
       if (err || !user) {
-        const error = new Error('Invalid login data!');
-        return next(error);
+        const error = new Error('Invalid login data!')
+        return next(error)
       }
       req.login(user, { session: false }, async (error: any) => {
-        if (error) return next(error);
-        const body = { userID: user?._id };
-        const token = jwt.sign({ user: body }, process.env.ACCESS_TOKEN_SECRET || '');
-        const { password, ...data } = user._doc; // don't send password field in response
-        return res.json({ token, user: data });
-      });
+        if (error) return next(error)
+        const body = { userID: user?._id }
+        const token = jwt.sign({ user: body }, process.env.ACCESS_TOKEN_SECRET || '', { expiresIn: '1d' })
+        const { password, ...data } = user._doc // don't send password field in response
+        return res.json({ token, user: data })
+      })
     } catch (error) {
-      return next(error);
+      return next(error)
     }
-  })(req, res, next);
-};
+  })(req, res, next)
+}
 
 const loggedUser = async (req: any, res: any) => {
   try {
-    const user = await User.findById(req.user.userID);
-    res.send({ user });
+    const user = await User.findById(req.user.userID)
+    res.send({ user })
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error)
   }
-};
+}
 
 const logout = (req: any, res: any) => {
   try {
-    req.logout();
-    res.send('Logged out succesfully!');
+    req.logout()
+    res.send('Logged out succesfully!')
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error)
   }
-};
+}
 
-export { login, loggedUser, logout };
+export { login, loggedUser, logout }
