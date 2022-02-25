3cd4857640bb54d04cb9928be96d7e7027699b5a
rilindaxhaqkaj <rilinda@centralsoft.io>
2022-02-25 23:46:10 +0100

    fix:student-atributtes

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,5 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 var UserSchema = new mongoose.Schema(
   {
@@ -32,22 +32,27 @@ var UserSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
+    gender: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
   {
     timestamps: true,
     collection: 'users',
     discriminatorKey: 'role',
   },
-);
+)
 
 UserSchema.pre('save', async function (next) {
-  const hashedPsw = await bcrypt.hash(this.password, 10);
-  this.password = hashedPsw;
-  next();
-});
+  const hashedPsw = await bcrypt.hash(this.password, 10)
+  this.password = hashedPsw
+  next()
+})
 
 UserSchema.virtual('fullName').get(function (this: any) {
-  return this.firstName + ' ' + this.lastName;
-});
+  return this.firstName + ' ' + this.lastName
+})
 
-export default mongoose.model('User', UserSchema);
+export default mongoose.model('User', UserSchema)
--- src/validators/student.validation.ts
+++ src/validators/student.validation.ts
@@ -1,12 +1,32 @@
-import Joi from 'joi';
-import { registerSchema, updateSchema } from './user.validation';
+import Joi from 'joi'
+import { registerSchema, updateSchema } from './user.validation'
 
 export const studentRegister = registerSchema.keys({
   studentId: Joi.number().required().label('Student ID'),
-  parent: Joi.string().label('Parent name'),
-});
+  parent: Joi.object({
+    firstName: Joi.string().required().label('Parent name'),
+    phoneNumber: Joi.number().required().label('Parent Number'),
+  }),
+  enrolledYear: Joi.number().required().label('Enrolled Year'),
+  nationality: Joi.string().required().label('Nationality'),
+  citizenship: Joi.string().required().label('Citizenship'),
+  place: Joi.string().required().label('Place'),
+  zipCode: Joi.number().required().label('Zipcode'),
+  personalEmail: Joi.string().required().label('Personal email'),
+  status: Joi.boolean().required().label('Status'),
+})
 
 export const studentUpdate = updateSchema.keys({
   studentId: Joi.number().label('Student ID'),
-  parent: Joi.string().label('Parent name'),
-});
+  parent: Joi.object({
+    firstName: Joi.string().label('Parent name'),
+    phoneNumber: Joi.number().label('Parent Number'),
+  }),
+  enrolledYear: Joi.number().label('Enrolled Year'),
+  nationality: Joi.string().label('Nationality'),
+  citizenship: Joi.string().label('Citizenship'),
+  place: Joi.string().label('Place'),
+  zipCode: Joi.number().label('Zipcode'),
+  personalEmail: Joi.string().label('Personal email'),
+  status: Joi.boolean().label('Status'),
+})
