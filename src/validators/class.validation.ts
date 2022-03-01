6c9d8625de7ac2c9d5701dcc5d5d454dc29e83eb
rilindaxhaqkaj <rilinda@centralsoft.io>
2022-03-02 00:05:27 +0100

    fix: class-capacity-validation

--- src/validators/class.validation.ts
+++ src/validators/class.validation.ts
@@ -3,7 +3,7 @@ import Joi from 'joi'
 export const registerSchema = Joi.object({
   level: Joi.number().min(1).max(12).required().label('Level'),
   className: Joi.string().min(3).max(100).required().label('Classname'),
-  classCapacity: Joi.number().min(2).max(30).required().label('Capacity'),
+  classCapacity: Joi.number().min(2).max(200).required().label('Capacity'),
   parallels: Joi.array().items({
     parallel: Joi.string().required().label('Parallel ID'),
   }),
@@ -13,7 +13,7 @@ export const updateSchema = Joi.object({
   classId: Joi.string().required().label('ID'),
   level: Joi.number().min(1).max(12).label('Level'),
   className: Joi.string().min(3).max(100).label('Classname'),
-  classCapacity: Joi.number().min(2).max(30).label('Capacity'),
+  classCapacity: Joi.number().min(2).max(200).label('Capacity'),
   parallels: Joi.array().items({
     parallel: Joi.string().label('Parallel ID'),
   }),
