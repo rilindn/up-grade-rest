35b3394c8c39c66bae37d77cc651d85e38797cea
rilind nuha <rn48707@ubt-uni.net>
2022-03-05 19:47:21 +0100

    fix: redefine model, validation and register the route

--- src/validators/remark.validation.ts
+++ src/validators/remark.validation.ts
@@ -1,19 +1,26 @@
 import Joi from 'joi'
 
 export const registerSchema = Joi.object({
-    teacher: Joi.object({
-      name: Joi.string().required().label('Teacher name'),
-    }),
-    remark: Joi.object({
-      description: Joi.string().required().label('Description'),
-    }),
-  })
+  teacher: Joi.object({
+    name: Joi.string().required().label('Teacher name'),
+    id: Joi.string().required().label('Teacher ID'),
+  }),
+  student: Joi.object({
+    name: Joi.string().required().label('Student name'),
+    id: Joi.string().required().label('Student ID'),
+  }),
+  description: Joi.string().required().label('Description'),
+})
 
-  export const updateSchema = Joi.object({
-    teacher: Joi.object({
-        name: Joi.string().required().label('Teacher name'),
-      }),
-      remark: Joi.object({
-        description: Joi.string().required().label('Description'),
-      }),
-  })
+export const updateSchema = Joi.object({
+  remarkId: Joi.string().required().label('Remark ID'),
+  teacher: Joi.object({
+    name: Joi.string().label('Teacher name'),
+    id: Joi.string().label('Teacher ID'),
+  }),
+  student: Joi.object({
+    name: Joi.string().label('Student name'),
+    id: Joi.string().label('Student ID'),
+  }),
+  description: Joi.string().label('Description'),
+})
